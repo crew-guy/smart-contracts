51db21a5294d9dcb95ba0d7207c45dd548a54cdf
ankit <ankit.sanghavi87@gmail.com>
2022-01-09 18:42:02 +0530

    add tailwind config

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center' >
+            <div className='animate-spin rounded-full h-32 w-32 border-b-2  border-red-700' ></div>
         </div>
     )
 }
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,10 +23,14 @@ const Welcome = () => {
     const connectWallet = () => {
 
     }
+
+    const handleSubmit = () => {
+
+    }
     return (
         <div className='flex w-full justify-center items-center' > 
-            <div className='flex md:flex-row flex-col justify-between items-start md:p-20 py-12 px-4 ' >
-                <div className='flex flex-1 justify-start flex-col md:mr-10 ' >
+            <div className='flex mf:flex-row flex-col justify-between items-start md:p-20 py-12 px-4 ' >
+                <div className='flex flex-1 justify-start flex-col mf:mr-10 ' >
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1' >Send Crypto <br /> across the world </h1>
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base' >
                         Explore the crypto world. Buy & easily sell cryptocurrencies easily on Krypto. 
@@ -58,7 +62,7 @@ const Welcome = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col flex-1 justify-start items-center md:mt-0 mt-10 ">
+                <div className="flex flex-col flex-1 justify-start items-center mf:mt-0 mt-10 ">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
@@ -82,6 +86,17 @@ const Welcome = () => {
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}} />
                         <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={()=>{}} />
                         <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}} />
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+                        {false
+                            ? (<Loader />)
+                            : <button
+                                onClick={handleSubmit}
+                                className="rounded-full text-white mt-2 border-[1px] p-2 border-[#3d4f7c] w-full cursor-pointer"
+                                type="button"
+                            >
+                                Send Now
+                            </button>
+                        }
                     </div>
                 </div>
             </div>
