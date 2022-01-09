7fe64d4693a679116248b971c62125a43a7d0224
ankit <ankit.sanghavi87@gmail.com>
2022-01-09 17:55:07 +0530

    start welcome section

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { AiFillPlayCircle } from 'react-icons/ai'
+import {Loader} from './'
 
 const Welcome = () => {
     return (
-        <div>
-            
+        <div className='flex w-full justify-center items-center' > 
+            <div className='flex md:flex-row flex-col justify-between items-start md:p-20 py-12 px-4 ' >
+                <div className='flex flex-1 justify-start flex-col md:mr-10 ' >
+                    <h1>Send Crypto <br /> across the world </h1>
+                </div>
+            </div>
         </div>
     )
 }
