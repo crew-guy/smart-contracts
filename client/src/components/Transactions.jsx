220a6cc09195e85b7f18df45e334a2aae4da6adc
ankit <ankit.sanghavi87@gmail.com>
2022-01-10 15:54:29 +0530

    basic transaction page

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useContract } from '../context/TransactionContext'
 
 const Transactions = () => {
+    const {connectedAccount} = useContract()
+
     return (
-        <div>
-            
+        <div className="flex w-full justify-center gradient-bg-transactions items-center 2xl:px-20" >
+            <div className="flex flex-col py-12 px-4 justify-center items-center">
+                {connectedAccount ? 
+                    <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
+                    :<h3 className="text-white text-3xl text-center my-2">Login to see your transactions</h3>
+                }
+            </div>
         </div>
     )
 }
