57b45aa702fccce313992228bde350bdb9ff578d
ankit <ankit.sanghavi87@gmail.com>
2022-01-10 17:17:06 +0530

    finish transactions ui

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,41 +1,49 @@
 import React, { Fragment } from 'react'
 import { useContract } from '../context/TransactionContext'
+import  useGifFetch  from '../hooks/useGifFetch'
 import {transactions} from '../utils/dummyData'
 import { shortenAddress } from '../utils/shortenAddress'
 
-const TransactionCard = ({id, url, message, timestamp, addressTo, addressFrom, amount}) => {
+const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    const gifUrl = useGifFetch({ keyword });
+  
     return (
-        <div className="flex bg-[#181918] 
-            2xl:min-w-[450px]
-            2xl:max-w-[500px]
-            sm:max-w-[300px]
-            sm:min-w-[270px]
-            flex-col p-3 m-2 rounded-md hover:shadow-2xl
-        ">
-            <div className="flex flex-col items-center w-full mt-3">
-                <div className="w-full mb-6 p-2">
-                    <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noopener noreferrer" >
-                        <div className="text-white text-base">To : {shortenAddress(addressTo)} </div>
-                    </a>
-                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer" >
-                        <div className="text-white text-base">From : {shortenAddress(addressFrom)} </div>
-                    </a>
-                    <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-                        <p className="text-base text-[#37c7da] font-bold">{timestamp.toString()}</p>
-                    </div>
-                    <p className="text-white text-base">Amount : {amount} ETH</p>
-                    {message && (
-                        <>
-                            <br />
-                            <p className="text-white text-base">Message : {message}</p>
-                        </>
-                    )}
-                    
-                </div>
-            </div>
+      <div className="bg-[#181918] m-4 flex flex-1
+        2xl:min-w-[350px]
+        2xl:max-w-[400px]
+        sm:min-w-[230px]
+        sm:max-w-[300px]
+        min-w-full
+        flex-col p-3 rounded-md hover:shadow-2xl"
+      >
+        <div className="flex flex-col items-center w-full mt-3">
+          <div className="display-flex justify-start w-full mb-6 p-2">
+            <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+              <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+            </a>
+            <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+              <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
+            </a>
+            <p className="text-white text-base">Amount: {amount} ETH</p>
+            {message && (
+              <>
+                <br />
+                <p className="text-white text-base">Message: {message}</p>
+              </>
+            )}
+          </div>
+          <img
+            src={gifUrl || url}
+            alt="nature"
+            className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+          />
+          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+                    <p className="text-[#37c7da] font-bold">{timestamp.getDate().toString()},{(timestamp.getMonth()+1).toString()}, {timestamp.getFullYear().toString()}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  };
 
 const Transactions = () => {
     const {connectedAccount} = useContract()
@@ -47,7 +55,7 @@ const Transactions = () => {
                     <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
                     :<h3 className="text-white text-3xl text-center my-2">Login to see your transactions</h3>
                 }
-                <div className="flex flex-wrap items-center justify-start">
+                <div className="flex flex-wrap items-center justify-center">
                     {transactions.reverse().map((transaction, i) => <TransactionCard key={i} {...transaction}/>  )}
                 </div>
                 
