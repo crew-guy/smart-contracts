3457e36d84a88a291e68d5d850e36e2a56ef2d90
ankit <ankit.sanghavi87@gmail.com>
2022-01-10 16:20:00 +0530

    make transaction card

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,37 @@
 import React from 'react'
 import { useContract } from '../context/TransactionContext'
+import {transactions} from '../utils/dummyData'
+import { shortenAddress } from '../utils/shortenAddress'
+
+const TransactionCard = ({id, url, message, timestamp, addressTo, addressFrom, amount}) => {
+    return (
+        <div className="flex bg-[#181918] 
+            2xl:min-w-[450px]
+            2xl:max-w-[500px]
+            sm:max-w-[300px]
+            sm:min-w-[270px]
+            flex-col p-3 rounded-md hover:shadow-2xl
+        ">
+            <div className="flex flex-col items-center w-full m-3">
+                <div className="w-full mb-6 p-2">
+                    <a href={`https://ropsten.etherscan.io/address/${$addressTo}`} target="_blank" rel="noopener noreferrer" >
+                        <div className="text-white text-base">To : {shortenAddress(addressTo)} </div>
+                    </a>
+                    <a href={`https://ropsten.etherscan.io/address/${$addressFrom}`} target="_blank" rel="noopener noreferrer" >
+                        <div className="text-white text-base">From : {shortenAddress(addressFrom)} </div>
+                    </a>
+                    <p className="text-white text-base">Amount : {amount} ETH</p>
+                    {message && (
+                        <>
+                            <br />
+                            <p className="text-white text-base">Message : {message}</p>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const Transactions = () => {
     const {connectedAccount} = useContract()
@@ -11,6 +43,10 @@ const Transactions = () => {
                     <h3 className="text-white text-3xl text-center my-2">Latest Transactions</h3>
                     :<h3 className="text-white text-3xl text-center my-2">Login to see your transactions</h3>
                 }
+                <div className="flex flex-wrap items-center justify-start">
+                    {transactions.reverse().map((transaction, i) => <TransactionCard key={i} {...transaction}/>  )}
+                </div>
+                
             </div>
         </div>
     )
---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,47 @@
+export const transactions = [
+    {
+        id: 1,
+        url: "google.com",
+        message: "Transaction 1", 
+        timestamp: new Date(),
+        addressFrom: "0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61315",
+        amount: "0.00012",
+        addressTo:"0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61313"
+    },
+    {
+        id: 2,
+        url: "google.com",
+        message: "Transaction 2", 
+        timestamp: new Date(),
+        addressFrom: "0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61315",
+        amount: "0.00012",
+        addressTo:"0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61313"
+    },
+    {
+        id: 3,
+        url: "google.com",
+        message: "Transaction 3", 
+        timestamp: new Date(),
+        addressFrom: "0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61315",
+        amount: "0.00012",
+        addressTo:"0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61313"
+    },
+    {
+        id: 4,
+        url: "google.com",
+        message: "Transaction 4", 
+        timestamp: new Date(),
+        addressFrom: "0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61315",
+        amount: "0.00012",
+        addressTo:"0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61313"
+    },
+    {
+        id: 5,
+        url: "google.com",
+        message: "Transaction 5", 
+        timestamp: new Date(),
+        addressFrom: "0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61315",
+        amount: "0.00012",
+        addressTo:"0xEf8F83b799000Bd1F4aB6606Dd4A712Ab5a61313"
+    },
+]
