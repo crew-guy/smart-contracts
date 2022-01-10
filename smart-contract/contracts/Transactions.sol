e40733c7d8cd8a1a6f31982e8b75ee9fe9646bdf
ankit <ankit.sanghavi87@gmail.com>
2022-01-10 12:05:18 +0530

    perfect deploy

--- smart-contract/contracts/Transactions.sol
+++ smart-contract/contracts/Transactions.sol
@@ -4,33 +4,54 @@ pragma solidity ^0.8.0;
 contract Transactions {
     uint256 transactionCount;
 
-
-    struct TransferStruct{
+    struct TransferStruct {
         address sender;
         address receiver;
-        uint amount;
+        uint256 amount;
         string message;
         uint256 timestamp;
         string keyword;
     }
 
-    event Transfer(address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
- 
+    event Transfer(
+        address sender,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
     TransferStruct[] transactions;
 
-    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public{
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
         transactionCount += 1;
-        transactions.push(TransferStruct({sender}))
+        transactions.push(
+            TransferStruct({
+                sender: msg.sender,
+                receiver: receiver,
+                amount: amount,
+                message: message,
+                timestamp: block.timestamp,
+                keyword: keyword
+            })
+        );
     }
 
-    function getAllTransactions() public view returns (TransferStruct[] memory){
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
         return transactions;
     }
 
-    function getTransactionCount() public view returns (uint256){
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
-
 }
-
-
